e5063c1f53ad71bc1ac66bb515943f4f14a14fc3
arjinozceylan <arjin.ozceylan@std.ieu.edu.tr>
2026-03-16 03:07:22 +0300

    önceden oluşturulan iskelete basit metod eklemeleri ve demo bir maç simülasyonu oluşturuldu

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
-        GameManager gameManager = new GameManager();
-
-        // Select sport (example: Football)
-        ISport sport = new Football();
-        gameManager.selectSport(sport);
-
-        // Start new game
-        gameManager.startNewGame("My Team");
-
-        System.out.println("Season started with sport: " + sport.getSportName());
-
-        // Simple simulation loop
-        while (!gameManager.isSeasonFinished()) {
-
-            System.out.println("Training week...");
-            gameManager.trainUserTeam();
-
-            System.out.println("Playing next match...");
-            gameManager.playNextWeek();
-
-            System.out.println("Current standings:");
-
-            League league = gameManager.getLeague();
-
-            for (Team team : league.getStandings()) {
-                System.out.println(team.getName() + " - " + team.getPoints() + " pts");
-            }
-
-            System.out.println("----------------------");
-        }
-
-        System.out.println("Season finished!");
+        UIManager uiManager = new UIManager();
+        uiManager.start();
     }
 }
